3476b033ec126a4a9dfbba357cd7c36d79ef8e42
HOYEONNN <96942402+HOYEONNN@users.noreply.github.com>
2025-11-26 15:52:57 +0700

    Update code

--- PerformanceTesting/StressTest/StressTest.js
+++ PerformanceTesting/StressTest/StressTest.js
@@ -1,33 +1,26 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-// Ensure this URL points to the product page you are testing
-const PRODUCT_URL = 'http://10.34.112.158:8000/gb';
+const PRODUCT_URL = 'http://10.34.112.158:8000/dk/store';
 
 export default function () {
-  // Simulate a user accessing a single, resource-intensive page
+  
   let res = http.get(PRODUCT_URL);
   
-  // Checks to ensure the request was successful
   check(res, {
     'status is 200': (r) => r.status === 200,
-    'response time is < 15s': (r) => r.timings.duration < 15000,
+    'response time is < 1s': (r) => r.timings.duration < 15000,
   });
 
-  // User "think time" between actions to simulate realistic behavior
   sleep(1); 
 }
 
-// ---------------------------------------------------------------
-// Stress Test Options (Find the Breakpoint)
-// ---------------------------------------------------------------
-
 export let options = {
   stages: [
-    { duration: '2m', target: 20 },  // Gradual increase to expected maximum
-    { duration: '2m', target: 50 },  // Pushing past expected limit (Stress Zone)
-    { duration: '2m', target: 75 },  // Reaching the potential breaking point
-    { duration: '2m', target: 0 },    // Cool-down period
+    { duration: '2m', target: 20 },  
+    { duration: '2m', target: 50 }, 
+    { duration: '2m', target: 75 }, 
+    { duration: '2m', target: 0 },    
   ],
   tags: { test_type: 'stress' },
 };
