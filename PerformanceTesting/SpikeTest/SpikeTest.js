a66816d1416f52803b64b4c40a83e1c23fe84e2e
HOYEONNN <96942402+HOYEONNN@users.noreply.github.com>
2025-11-26 15:50:55 +0700

    Update code

--- PerformanceTesting/SpikeTest/SpikeTest.js
+++ PerformanceTesting/SpikeTest/SpikeTest.js
@@ -1,32 +1,25 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-// Ensure this URL points to the product page you are testing
-const PRODUCT_URL = 'http://10.34.112.158:8000/gb';
+const PRODUCT_URL = 'http://10.34.112.158:8000/dk/store';
 
 export default function () {
-  // Simulate a user accessing a single, resource-intensive page
+ 
   let res = http.get(PRODUCT_URL);
   
-  // Checks to ensure the request was successful
   check(res, {
     'status is 200': (r) => r.status === 200,
     'response time is < 15s': (r) => r.timings.duration < 15000,
   });
 
-  // User "think time" between actions to simulate realistic behavior
   sleep(1); 
 }
 
-// ---------------------------------------------------------------
-// Spike Test Options (Test Resilience and Recovery Time)
-// ---------------------------------------------------------------
-
 export let options = {
   stages: [
-    { duration: '2m', target: 10 },   // Establish Base Load
-    { duration: '2m', target: 70 },  // SUDDEN SPIKE! (5x the base load)
-    { duration: '2m', target: 40 },   // Recovery back to Base Load
+    { duration: '2m', target: 10 },
+    { duration: '2m', target: 70 },  
+    { duration: '2m', target: 40 }, 
   ],
   tags: { test_type: 'spike' },
 };
